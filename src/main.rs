3f81d0e4fedc5dccd0aefb0a712e1cefc2a10890
caverym <averylapine@gmail.com>
2022-06-21 02:52:03 -0700

    Bases + Warning

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use bevy::{
+    prelude::*,
+    DefaultPlugins,
+};
+
+mod warning;
+
+#[derive(Clone, PartialEq, Eq, Debug, Hash)]
+pub enum GameFrames {
+    Frame17,
+    Title,
+    WhatDay,
+    Frame1,
+    Died,
+    Freddy,
+    NextDay,
+    Wait,
+    GameOver,
+    TheEnd,
+    Ad,
+    TheEnd2,
+    Customize,
+    TheEnd3,
+    CreepyStart,
+    CreepyEnd,
+    EndOfDemo,
+}
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
+        .add_plugins(DefaultPlugins)
+        // .add_plugin(TiledCameraPlugin)
+        .add_startup_system(setup)
+        .add_state(GameFrames::Frame17)
+        .add_plugin(warning::WarningPlugin)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle(UiCameraBundle::default());
+    // commands.spawn_bundle(TiledCameraBundle::new().with_target_resolution(1, [1280, 720]));
+}
+
+pub fn despawn_screen<T: Component>(to_despawn: Query<Entity, With<T>>, mut commands: Commands) {
+    for entity in to_despawn.iter() {
+        commands.entity(entity).despawn_recursive();
+    }
+}
---
+++ src/warning.rs
@@ -0,0 +1,114 @@
+use bevy::prelude::*;
+
+use crate::{despawn_screen, GameFrames};
+
+pub struct WarningPlugin;
+
+impl Plugin for WarningPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_system_set(SystemSet::on_enter(GameFrames::Frame17).with_system(setup))
+            .add_system_set(
+                SystemSet::on_update(GameFrames::Frame17)
+                .with_system(wait_to_fade)
+                    .with_system(fade)
+                    .with_system(countdown)
+                    .with_system(keyboard)
+                    .with_system(mouse),
+            )
+            .add_system_set(
+                SystemSet::on_exit(GameFrames::Frame17)
+                    .with_system(despawn_screen::<OnWarningScreen>),
+            );
+    }
+}
+
+#[derive(Component)]
+pub struct WarningScreen;
+
+#[derive(Component)]
+pub struct OnWarningScreen;
+
+#[derive(Deref, DerefMut)]
+pub struct WarningTimer(Timer);
+
+#[derive(Deref, DerefMut)]
+pub struct FadeStartTimer(Timer);
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let warning_text = asset_server.load("images/[0] 'Frame 17'/[unsorted]/605.png");
+
+    commands
+        .spawn_bundle(ImageBundle {
+            color: UiColor(Color::rgba(1.0, 1.0, 1.0, 1.0)),
+            image: UiImage(warning_text),
+            style: Style {
+                margin: Rect {
+                    top: Val::Px(249.0),
+                    left: Val::Px(426.0),
+                    bottom: Val::Px(373.0),
+                    ..default()
+                },
+                // margin: Rect { top: Val::Px(249.0), left: Val::Px(426.0), ..default()}, // right: Val::Px(891.0), ..default() }, // bottom: Val::Px(373.0) },
+                size: Size::new(Val::Px(465.0), Val::Px(124.0)),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(OnWarningScreen);
+
+    commands.insert_resource(WarningTimer(Timer::from_seconds(3.01, false)));
+    commands.insert_resource(FadeStartTimer(Timer::from_seconds(2.0, false)));
+    commands.insert_resource(FadeTimer(Timer::from_seconds(1.1, false)));
+}
+
+#[derive(Deref, DerefMut)]
+pub struct FadeTimer(Timer);
+
+fn countdown(
+    mut game_state: ResMut<State<GameFrames>>,
+    time: Res<Time>,
+    mut timer: ResMut<WarningTimer>,
+) {
+    if timer.tick(time.delta()).finished() {
+        game_state.set(GameFrames::Title).unwrap();
+    }
+}
+
+fn wait_to_fade(
+    time: Res<Time>,
+    mut timer: ResMut<FadeStartTimer>,
+    mut fade_timer: ResMut<FadeTimer>,
+) {
+    if timer.tick(time.delta()).finished() {
+        fade_timer.tick(time.delta());
+    }
+}
+
+fn fade(
+    timer: ResMut<FadeTimer>,
+    mut query: Query<&mut UiColor>,
+) {
+    if timer.percent_left() == 1.0 { return; }
+
+    if timer.finished() {
+        for mut color in query.iter_mut() {
+            color.0.set_a(0.0);
+        }
+    }
+
+    for mut color in query.iter_mut() {
+        color.0.set_a(1.0 * timer.percent_left());
+    }
+}
+
+fn keyboard(mut game_state: ResMut<State<GameFrames>>, keys: Res<Input<KeyCode>>) {
+    if keys.just_pressed(KeyCode::Return) || keys.just_pressed(KeyCode::NumpadEnter) {
+        game_state.set(GameFrames::Title).unwrap();
+    }
+}
+
+fn mouse(mut game_state: ResMut<State<GameFrames>>, mouse: Res<Input<MouseButton>>) {
+    if mouse.just_pressed(MouseButton::Left) {
+        game_state.set(GameFrames::Title).unwrap();
+    }
+}
